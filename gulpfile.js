71d11e630127c4708d0ea14aeb8c5e74d51091fe
William Bettencourt <williambet@gmail.com>
2015-12-24 15:22:13 -0500

    reorganizing code in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,9 @@ gulp.task("lint", function(){
     .pipe(jshint.reporter("fail"));
 });
 
+/* Gulp tasks for Development Environment */
+
 gulp.task("clean-dev", function(){
-    return del("./dist.dev/");
-});
-gulp.task("clean-prod", function(){
     return del("./dist.dev/");
 });
 
@@ -29,14 +28,6 @@ gulp.task("copy-css-dev", ["clean-dev"], function(){
     gulp.src(["./app/**/*.css"])
     .pipe(gulp.dest("./dist.dev/"));
 });
-
-gulp.task("copy-css-prod", ["clean-prod"], function(){
-    var opts = { comments: true, spare: true };
-    gulp.src(["./app/**/*.css"])
-    .pipe(minifyCSS(opts))
-    .pipe(gulp.dest("./dist.prod/"));
-});
-    
 
 gulp.task("browserify-dev", ["clean-dev"], function(){
     var opts = { debug: true }
@@ -46,33 +37,14 @@ gulp.task("browserify-dev", ["clean-dev"], function(){
         .pipe(gulp.dest("./dist.dev/js/"));
 });
 
-gulp.task("browserify-prod", ["clean-prod"], function(){
-    return browserify("./app/js/main.js")
-        .bundle()
-        .pipe(source("bundled.min.js"))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist.prod/js/"))
-});
-
 gulp.task("copy-bower-components-dev", ["clean-dev"], function(){
     return gulp.src(bowerFiles())
         .pipe(gulp.dest("./dist.dev/vendor"));
 });
 
-gulp.task("copy-bower-components-prod", ["clean-prod"], function(){
-    return gulp.src(bowerFiles())
-        .pipe(gulp.dest("./dist.prod/vendor"));
-});
-
 gulp.task("copy-html-files-dev", ["clean-dev"], function(){
     gulp.src("./app/**/*.html")
     .pipe(gulp.dest("./dist.dev/"));
-});
-
-gulp.task("copy-html-files-prod", ["clean-prod"], function(){
-    gulp.src("./app/**/*.html")
-    .pipe(gulp.dest("./dist.prod/"));
 });
 
 gulp.task("inject-dev", ["copy-css-dev", "copy-html-files-dev", "copy-bower-components-dev", "browserify-dev"], function(){
@@ -84,6 +56,38 @@ gulp.task("inject-dev", ["copy-css-dev", "copy-html-files-dev", "copy-bower-comp
         .pipe(gulp.dest("./dist.dev/"));
 });
 
+/* Gulp tasks for Production Environment */
+
+gulp.task("clean-prod", function(){
+    return del("./dist.dev/");
+});
+
+gulp.task("copy-css-prod", ["clean-prod"], function(){
+    var opts = { comments: true, spare: true };
+    gulp.src(["./app/**/*.css"])
+    .pipe(minifyCSS(opts))
+    .pipe(gulp.dest("./dist.prod/"));
+});
+
+gulp.task("browserify-prod", ["clean-prod"], function(){
+    return browserify("./app/js/main.js")
+        .bundle()
+        .pipe(source("bundled.min.js"))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist.prod/js/"))
+});
+
+gulp.task("copy-bower-components-prod", ["clean-prod"], function(){
+    return gulp.src(bowerFiles())
+        .pipe(gulp.dest("./dist.prod/vendor"));
+});
+
+gulp.task("copy-html-files-prod", ["clean-prod"], function(){
+    gulp.src("./app/**/*.html")
+    .pipe(gulp.dest("./dist.prod/"));
+});
+
 gulp.task("inject-prod", ["copy-css-prod", "copy-html-files-prod", "copy-bower-components-prod", "browserify-prod"], function(){
     var sources = gulp.src(["./dist.prod/js/**/*.js", "./dist.prod/**/*.css"]);
     var vendor = gulp.src(["./dist.prod/vendor/**/*"]).pipe(order(['jquery.js', 'angular.js', 'angular-animate.js', "angular-route.js"]));
@@ -93,6 +97,7 @@ gulp.task("inject-prod", ["copy-css-prod", "copy-html-files-prod", "copy-bower-c
         .pipe(gulp.dest("./dist.prod/"));
 });
 
+/* MAIN TASKS */
 
 gulp.task("connectDev", ["inject-dev"], function(){
     connect.server({
@@ -108,7 +113,7 @@ gulp.task("connectProd", ["inject-prod"], function(){
     });
 });
 
-gulp.task("default", ["build-dev"]);
-
 gulp.task("build-dev", ["connectDev"]);
 gulp.task("build-prod", ["connectProd"]);
+
+gulp.task("default", ["build-dev"]);
